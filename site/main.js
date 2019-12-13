39b978595e6d30df09ec5e75ffa76e3cdcf8d012
Elias <elias@bookpost.eu>
2019-12-13 13:20:57 +0100

    skapade hemsidan

---
+++ site/main.js
@@ -0,0 +1,5 @@
+function SliderChange(element) {
+	var elementValue = element.value;
+	var body = document.getElementById("body");
+	body.style.backgroundColor = "rgb(" + elementValue + "," + elementValue + "," + elementValue +")";
+}
